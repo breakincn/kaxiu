3217abd2c3cab2ea69ec39a7d6cc59c9fd0e1996
will <breakincn@gmail.com>
2025-12-29 14:57:09 +0800

    chore:配置kabao.me

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
+    allowedHosts: ['kabao.me', '.kabao.me'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
