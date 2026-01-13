9bdee13583cfd7c71e748fb5584ddf329a62538e
will <breakincn@gmail.com>
2026-01-13 10:50:15 +0800

    feat:本地运行支持局域网访问

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -17,6 +17,11 @@ func CORSMiddleware() cors.Config {
 		"http://127.0.0.1:3000",
 		"http://10.0.0.20:3000",
 		"http://10.0.0.20:5173", // Vite 默认端口
+		// 支持局域网访问
+		"http://10.0.0.*:3000",
+		"http://10.0.0.*:5173",
+		"http://192.168.*:3000",
+		"http://192.168.*:5173",
 	}
 	fmt.Println("DEBUG KABAO_CORS_ALLOW_ORIGINS =", originsEnv) // 打印环境变量
 	if originsEnv != "" {
